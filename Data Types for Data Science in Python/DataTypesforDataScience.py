686f60f20f5f3f232215ce133d351cc730c3bcc8
Alexis Enriquez <alexisenriquez1095@gmail.com>
2021-03-05 14:31:16 -0600

    Add files via upload

---
+++ Data Types for Data Science in Python/DataTypesforDataScience.py
@@ -0,0 +1,478 @@
+# Course 2
+# Data Types for Data Science in Python
+
+# Create a list containing the names: baby_names
+baby_names = ['Ximena','Aliza','Ayden','Calvin']
+
+# Extend baby_names with 'Rowen' and 'Sandeep'
+baby_names.extend(['Rowen','Sandeep'])
+
+# Print baby_names
+print(baby_names)
+
+# Find the position of 'Aliza': position
+position = baby_names.index('Aliza')
+
+# Remove 'Aliza' from baby_names
+baby_names.pop(position)
+
+# Print baby_names
+print(baby_names)
+
+# Create the empty list: baby_names
+baby_names = []
+
+# Loop over records 
+for row in records:
+    # Add the name to the list
+    baby_names.append(row[3])
+    
+# Sort the names in alphabetical order
+for name in sorted(baby_names):
+    # Print each name
+    print(name)
+
+# Pair up the girl and boy names: pairs
+pairs = list(zip(girl_names,boy_names))
+
+# Iterate over pairs
+for idx, pair in enumerate(pairs):
+    # Unpack pair: girl_name, boy_name
+    girl_name, boy_name = pair
+    # Print the rank and names associated with each rank
+    print('Rank {}: {} and {}'.format(idx, girl_name, boy_name))
+
+# Create the normal variable: normal
+normal = 'simple'
+
+# Create the mistaken variable: error
+error = ('trailing comma',)
+
+# Print the types of the variables
+print(type(normal))
+print(type(error))
+
+# Create an empty dictionary: names_by_rank
+names_by_rank = {}
+
+# Loop over the girl names
+for rank, name in female_baby_names_2012.items():
+    # Add each name to the names_by_rank dictionary using rank as the key
+    names_by_rank[rank] = name
+    
+# Sort the names_by_rank dict by rank in descending order and slice the first 10 items
+for rank in sorted(names_by_rank, reverse=True)[:10]:
+    # Print each item
+    print(names_by_rank[rank])
+
+# Assign the names_2011 dictionary as the value to the 2011 key of boy_names
+boy_names[2011] = names_2011
+
+# Update the 2012 key in the boy_names dictionary
+boy_names[2012].update([(1,'Casey'), (2,'Aiden')])
+
+# Loop over the years in the boy_names dictionary 
+for year in boy_names:
+    # sort the data for each year by descending rank and get the lowest one
+    lowest_ranked = sorted(boy_names[year], reverse=True)[0]
+    # Safely print the year and the least popular name or 'Not Available'
+    print(year, boy_names[year].get(lowest_ranked, 'Not Available'))
+
+# Iterate over the 2014 nested dictionary
+for rank, name in baby_names[2014].items():
+    # Print rank and name
+    print(rank, name)
+    
+# Iterate over the 2012 nested dictionary
+for rank, name in baby_names[2012].items():
+    # Print rank and name
+    print(rank, name)
+ 
+ # Import the python CSV module
+import csv
+
+# Create a python file object in read mode for the baby_names.csv file: csvfile
+csvfile = open('baby_names.csv', 'r')
+
+# Loop over a csv reader on the file object
+for row in csv.reader(csvfile):
+    # Print each row 
+    print(row)
+    # Add the rank and name to the dictionary
+    baby_names[row[5]] = row[3]
+
+# Print the dictionary keys
+print(baby_names.keys())
+
+# Import the python CSV module
+import csv
+
+# Create a python file object in read mode for the `baby_names.csv` file: csvfile
+csvfile =  open('baby_names.csv', 'r')
+
+# Loop over a DictReader on the file
+for row in csv.DictReader(csvfile):
+    # Print each row
+    print(row)
+    # Add the rank and name to the dictionary: baby_names
+    baby_names[row['RANK']] = row['NAME']
+
+# Print the dictionary
+print(baby_names.keys())
+
+# Import the Counter object
+from collections import Counter
+
+# Print the first ten items from the stations list
+print(stations[:10])
+
+# Create a Counter of the stations list: station_count
+station_count = Counter(stations)
+
+# Print the station_count
+print(station_count)
+
+# Import the Counter object
+from collections import Counter
+
+# Create a Counter of the stations list: station_count
+station_count = Counter(stations)
+
+# Find the 5 most common elements
+print(station_count.most_common(5))
+
+# Create an empty dictionary: ridership
+ridership = {}
+
+# Iterate over the entries
+for date, stop, riders in entries:
+    # Check to see if date is already in the ridership dictionary
+    if date not in ridership:
+        # Create an empty list for any missing date
+        ridership[date] = []
+    # Append the stop and riders as a tuple to the date keys list
+    ridership[date].append((stop,riders))
+    
+# Print the ridership for '03/09/2016'
+print(ridership['03/09/2016'])
+
+# Import defaultdict
+from collections import defaultdict
+
+# Create a defaultdict with a default type of list: ridership
+ridership = defaultdict(list)
+
+# Iterate over the entries
+for date, stop, riders in entries:
+    # Use the stop as the key of ridership and append the riders to its value
+    ridership[stop].append(riders)
+    
+# Print the first 10 items of the ridership dictionary
+print(list(ridership.items())[:10])
+
+# Import OrderedDict from collections
+from collections import OrderedDict
+
+# Create an OrderedDict called: ridership_date
+ridership_date = OrderedDict()
+
+# Iterate over the entries
+for date, riders in entries:
+    # If a key does not exist in ridership_date, set it to 0
+    if date not in ridership_date:
+        ridership_date[date] = 0
+        
+    # Add riders to the date key in ridership_date
+    ridership_date[date] += riders
+    
+# Print the first 31 records
+print(list(ridership_date.items())[:31])
+
+# Print the first key in ridership_date
+print(list(ridership_date.keys())[0])
+
+# Pop the first item from ridership_date and print it
+print(ridership_date.popitem(last=False))
+
+# Print the last key in ridership_date
+print(list(ridership_date.keys())[-1])
+
+# Pop the last item from ridership_date and print it
+print(ridership_date.popitem())
+
+# Import namedtuple from collections
+from collections import namedtuple
+
+# Create the namedtuple: DateDetails
+DateDetails = namedtuple('DateDetails', ['date', 'stop', 'riders'])
+
+# Create the empty list: labeled_entries
+labeled_entries = []
+
+# Iterate over the entries list
+for date, stop, riders in entries:
+    # Append a new DateDetails namedtuple instance for each entry to labeled_entries
+    labeled_entries.append(DateDetails(date, stop, riders))
+    
+# Print the first 5 items in labeled_entries
+print(labeled_entries[:5])
+
+# Iterate over the first twenty items in labeled_entries
+for item in labeled_entries[:20]:
+    # Print each item's stop
+    print(item.stop)
+
+    # Print each item's date
+    print(item.date)
+
+    # Print each item's riders
+    print(item.riders)
+
+# Import the datetime object from datetime
+from datetime import datetime
+
+# Iterate over the dates_list 
+for date_str in dates_list:
+    # Convert each date to a datetime object: date_dt
+    date_dt = datetime.strptime(date_str,'%m/%d/%Y')
+    
+    # Print each date_dt
+    print(date_dt)
+
+# Loop over the first 10 items of the datetimes_list
+for item in datetimes_list[:10]:
+    # Print out the record as a string in the format of 'MM/DD/YYYY'
+    print(item.strftime('%m/%d/%Y'))
+    
+    # Print out the record as an ISO standard string
+    print(item.isoformat())
+
+# Create a defaultdict of an integer: monthly_total_rides
+monthly_total_rides = defaultdict(int)
+
+# Loop over the list daily_summaries
+for daily_summary in daily_summaries:
+    # Convert the service_date to a datetime object
+    service_datetime = datetime.strptime(daily_summary[0],'%m/%d/%Y')
+
+    # Add the total rides to the current amount for the month
+    monthly_total_rides[service_datetime.month] += int(daily_summary[4])
+    
+# Print monthly_total_rides
+print(monthly_total_rides)
+
+# Import datetime from the datetime module
+from datetime import datetime
+
+# Compute the local datetime: local_dt
+local_dt = datetime.now()
+
+# Print the local datetime
+print(local_dt)
+
+# Compute the UTC datetime: utc_dt
+utc_dt = datetime.utcnow()
+
+# Print the UTC datetime
+print(utc_dt)
+
+# Create a Timezone object for Chicago
+chicago_usa_tz = timezone('US/Central')
+
+# Create a Timezone object for New York
+ny_usa_tz = timezone('US/Eastern')
+
+# Iterate over the daily_summaries list
+for orig_dt, ridership in daily_summaries:
+
+    # Make the orig_dt timezone "aware" for Chicago
+    chicago_dt = orig_dt.replace(tzinfo=chicago_usa_tz)
+    
+    # Convert chicago_dt to the New York Timezone
+    ny_dt = chicago_dt.astimezone(ny_usa_tz)
+    
+    # Print the chicago_dt, ny_dt, and ridership
+    print('Chicago: %s, NY: %s, Ridership: %s' % (chicago_dt, ny_dt, ridership))
+
+# Import timedelta from the datetime module
+from datetime import timedelta
+
+# Build a timedelta of 30 days: glanceback
+glanceback = timedelta(days = 30)
+
+# Iterate over the review_dates as date
+for date in review_dates:
+    # Calculate the date 30 days back: prior_period_dt
+    prior_period_dt = date - glanceback
+    
+    # Print the review_date, day_type and total_ridership
+    print('Date: %s, Type: %s, Total Ridership: %s' %
+         (date, 
+          daily_summaries[date]['day_type'], 
+          daily_summaries[date]['total_ridership']))
+
+    # Print the prior_period_dt, day_type and total_ridership
+    print('Date: %s, Type: %s, Total Ridership: %s' %
+         (prior_period_dt, 
+          daily_summaries[prior_period_dt]['day_type'], 
+          daily_summaries[prior_period_dt]['total_ridership']))
+# Iterate over the date_ranges
+for start_date, end_date in date_ranges:
+    # Print the End and Start Date
+    print(end_date, start_date)
+    # Print the difference between each end and start date
+    print(end_date-start_date)
+
+# Import the pendulum module
+import pendulum
+
+# Create a now datetime for Tokyo: tokyo_dt
+tokyo_dt = pendulum.now('Asia/Tokyo')
+
+# Covert the tokyo_dt to Los Angeles: la_dt
+la_dt = tokyo_dt.in_timezone('America/Los_Angeles')
+
+# Print the ISO 8601 string of la_dt
+print(la_dt.to_iso8601_string())
+
+# Iterate over date_ranges
+for start_date, end_date in date_ranges:
+
+    # Convert the start_date string to a pendulum date: start_dt 
+    start_dt = pendulum.parse(start_date, strict = False)
+    
+    # Convert the end_date string to a pendulum date: end_dt 
+    end_dt = pendulum.parse(end_date, strict = False)
+    
+    # Print the End and Start Date
+    print(end_dt, start_dt)
+    
+    # Calculate the difference between end_dt and start_dt: diff_period
+    diff_period = end_dt - start_dt
+    
+    # Print the difference in days
+    print(diff_period.in_days())
+
+# Chapter 5
+# Import the csv module
+import csv
+
+# Create the file object: csvfile
+csvfile = open('crime_sampler.csv','r')
+
+# Create an empty list: crime_data
+crime_data = []
+
+# Loop over a csv reader on the file object
+for row in csv.reader(csvfile):
+
+    # Append the date, type of crime, location description, and arrest
+    crime_data.append((row[0], row[2], row[4], row[5]))
+    
+# Remove the first element from crime_data
+crime_data.pop(0)
+
+# Print the first 10 records
+print(crime_data[:10])
+
+# Import necessary modules
+from collections import Counter
+from datetime import datetime
+
+# Create a Counter Object: crimes_by_month
+crimes_by_month = Counter()
+
+# Loop over the crime_data list
+for date in crime_data:
+    
+    # Convert the first element of each item into a Python Datetime Object: date
+    date = datetime.strptime(date[0], '%m/%d/%Y %I:%M:%S %p')
+    
+    # Increment the counter for the month of the row by one
+    crimes_by_month[date.month] += 1
+    
+# Print the 3 most common months for crime
+print(crimes_by_month.most_common(3))
+
+# Import necessary modules
+from collections import defaultdict
+from datetime import datetime
+
+# Create a dictionary that defaults to a list: locations_by_month
+locations_by_month = defaultdict(list)
+
+# Loop over the crime_data list
+for row in crime_data:
+    # Convert the first element to a date object
+    date = datetime.strptime(row[0], '%m/%d/%Y %I:%M:%S %p')
+    
+    # If the year is 2016 
+    if date.year == 2016:
+        # Set the dictionary key to the month and append the location (fifth element) to the values list
+        locations_by_month[date.month].append(row[4])
+    
+# Print the dictionary
+print(locations_by_month)
+
+# Import Counter from collections
+from collections import Counter
+
+# Loop over the items from locations_by_month using tuple expansion of the month and locations
+for month, locations in locations_by_month.items():
+    # Make a Counter of the locations
+    location_count = Counter(locations)
+    # Print the month 
+    print(month)
+    # Print the most common location
+    print(location_count.most_common(5))
+
+# Create the CSV file: csvfile
+csvfile = open('crime_sampler.csv','r')
+
+# Create a dictionary that defaults to a list: crimes_by_district
+crimes_by_district = defaultdict(list)
+
+# Loop over a DictReader of the CSV file
+for row in csv.DictReader(csvfile):
+    # Pop the district from each row: district
+    district = row.pop('District')
+    # Append the rest of the data to the list for proper district in crimes_by_district
+    crimes_by_district[district].append(row)
+
+# Loop over the crimes_by_district using expansion as district and crimes
+for district, crimes in crimes_by_district.items():
+    # Print the district
+    print(district)
+    
+    # Create an empty Counter object: year_count
+    year_count = Counter()
+    
+    # Loop over the crimes:
+    for crime in crimes:
+        # If there was an arrest
+        if crime['Arrest'] == 'true':
+            # Convert the Date to a datetime and get the year
+            year = datetime.strptime(crime['Date'], '%m/%d/%Y %I:%M:%S %p').year
+            # Increment the Counter for the year
+            year_count[year] += 1
+            
+    # Print the counter
+    print(year_count)
+
+    # Create a unique list of crimes for the first block: n_state_st_crimes
+n_state_st_crimes = set(crimes_by_block['001XX N STATE ST'])
+
+# Print the list
+print(n_state_st_crimes)
+
+# Create a unique list of crimes for the second block: w_terminal_st_crimes
+w_terminal_st_crimes = set(crimes_by_block['0000X W TERMINAL ST'])
+
+# Print the list
+print(w_terminal_st_crimes)
+
+# Find the differences between the two blocks: crime_differences
+crime_differences = n_state_st_crimes.difference(w_terminal_st_crimes)
+
+# Print the differences
+print(crime_differences)
